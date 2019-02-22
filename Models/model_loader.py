c5a389feb786cce43274cdcf6ac0b7426eafcc7b
Lanxin-95 <675579465@qq.com>
2019-02-22 21:39:07 +0800

    add model_loader

--- Models/model_loader.py
+++ Models/model_loader.py
@@ -1,3 +1,20 @@
 from FCN32s import FCN32s
+import torch
+from torchvision import models
 
 
+def model_loader(model_name, n_classes, resume):
+    if model_name == 'FCN32s':
+        model = FCN32s(n_classes=n_classes)
+
+    start_epoch = 1
+    if resume:
+        checkpoint = torch.load(resume)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        start_epoch = checkpoint['epoch']
+    else:
+        if model_name == 'FCN32s':
+            vgg16 = models.vgg16(pretrained=True)
+            model.copy_params_from_vgg16(vgg16)
+
+    return model, start_epoch, checkpoint
