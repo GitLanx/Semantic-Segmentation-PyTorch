77a5b449eccd9b273bf329065610a0bfd5dec411
Lanxin-95 <675579465@qq.com>
2019-02-23 09:45:09 +0800

    add fcn8s

--- Models/model_loader.py
+++ Models/model_loader.py
@@ -1,19 +1,22 @@
-from FCN32s import FCN32s
+from .FCN import FCN32s, FCN8sAtOnce
 import torch
 from torchvision import models
 
 
 def model_loader(model_name, n_classes, resume):
-    if model_name == 'FCN32s':
+    if model_name == 'fcn32s':
         model = FCN32s(n_classes=n_classes)
-
+    elif model_name == 'fcn8s':
+        model = FCN8sAtOnce(n_classes=n_classes)
+    else:
+        raise ValueError('Unsupported model')
     start_epoch = 1
     if resume:
         checkpoint = torch.load(resume)
         model.load_state_dict(checkpoint['model_state_dict'])
         start_epoch = checkpoint['epoch']
     else:
-        if model_name == 'FCN32s':
+        if model_name in ['fcn32s', 'fcn8s']:
             vgg16 = models.vgg16(pretrained=True)
             model.copy_params_from_vgg16(vgg16)
 
