09325deb139d30f2843a3df8e5b7b1c984a33ced
GitLanx <675579465@qq.com>
2019-03-06 15:59:18 +0800

    add pretrained vgg

--- Models/SegNet.py
+++ Models/SegNet.py
@@ -1,5 +1,3 @@
-import numpy as np
-import torch
 import torch.nn as nn
 
 
@@ -27,7 +25,7 @@ class DeconvBlock(nn.Module):
         super(DeconvBlock, self).__init__()
         layers = [
             nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
+            nn.BatchNorm2d(in_channels),
             nn.ReLU(inplace=True)
         ] * (n_blocks - 1)
         layers += [
@@ -47,105 +45,95 @@ class SegNet(nn.Module):
         # conv1
         self.conv1 = ConvBlock(3, 64, 2)
         self.pool1 = nn.MaxPool2d(2, stride=2, return_indices=True, ceil_mode=True)  # 1/2
-
         # conv2
         self.conv2 = ConvBlock(64, 128, 2)
         self.pool2 = nn.MaxPool2d(2, stride=2, return_indices=True, ceil_mode=True)  # 1/4
-
         # conv3
         self.conv3 = ConvBlock(128, 256, 3)
         self.pool3 = nn.MaxPool2d(2, stride=2, return_indices=True, ceil_mode=True)  # 1/8
-
         # conv4
         self.conv4 = ConvBlock(256, 512, 3)
         self.pool4 = nn.MaxPool2d(2, stride=2, return_indices=True, ceil_mode=True)  # 1/16
-
         # conv5
         self.conv5 = ConvBlock(512, 512, 3)
         self.pool5 = nn.MaxPool2d(2, stride=2, return_indices=True, ceil_mode=True)  # 1/32
 
         self.unpool6 = nn.MaxUnpool2d(2, stride=2)
         self.conv6_D = DeconvBlock(512, 512, 3)
-
         self.unpool7 = nn.MaxUnpool2d(2, stride=2)
         self.conv7_D = DeconvBlock(512, 256, 3)
-
         self.unpool8 = nn.MaxUnpool2d(2, stride=2)
         self.conv8_D = DeconvBlock(256, 128, 3)
-
         self.unpool9 = nn.MaxUnpool2d(2, stride=2)
         self.conv9_D = DeconvBlock(128, 64, 2)
 
         self.unpool10 = nn.MaxUnpool2d(2, stride=2)
-        self.final = nn.Sequential([
+        self.final = nn.Sequential(
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, n_classes, kernel_size=3, padding=1),
-        ])
+        )
+
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.fill_(0.001)
 
     def forward(self, x):
-        h = x
-        h = self.conv1(h)
-        h, indices_1 = self.pool1(h)
+        out = self.conv1(x)
+        out, indices_1 = self.pool1(out)
 
-        h = self.conv2(h)
-        h, indices_2 = self.pool2(h)
+        out = self.conv2(out)
+        out, indices_2 = self.pool2(out)
 
-        h = self.conv3(h)
-        h, indices_3 = self.pool3(h)
+        out = self.conv3(out)
+        out, indices_3 = self.pool3(out)
 
-        h = self.conv4(h)
-        h, indices_4 = self.pool4(h)
+        out = self.conv4(out)
+        out, indices_4 = self.pool4(out)
 
-        h = self.conv5(h)
-        h, indices_5 = self.pool5(h)
+        out = self.conv5(out)
+        out, indices_5 = self.pool5(out)
 
-        h = self.unpool6(h, indices_5)
-        h = self.conv6_D(h)
+        out = self.unpool6(out, indices_5)
+        out = self.conv6_D(out)
 
-        h = self.unpool7(h, indices_4)
-        h = self.conv7_D(h)
+        out = self.unpool7(out, indices_4)
+        out = self.conv7_D(out)
 
-        h = self.unpool8(h, indices_3)
-        h = self.conv8_D(h)
+        out = self.unpool8(out, indices_3)
+        out = self.conv8_D(out)
 
-        h = self.unpool9(h, indices_2)
-        h = self.conv9_D(h)
+        out = self.unpool9(out, indices_2)
+        out = self.conv9_D(out)
 
-        h = self.unpool10(h, indices_1)
-        h = self.final(h)
-        return h
+        out = self.unpool10(out, indices_1)
+        out = self.final(out)
+        return out
 
     def copy_params_from_vgg16(self, vgg16):
-        features = [
-            self.conv1_1, self.relu1_1,
-            self.conv1_2, self.relu1_2,
-            self.pool1,
-            self.conv2_1, self.relu2_1,
-            self.conv2_2, self.relu2_2,
-            self.pool2,
-            self.conv3_1, self.relu3_1,
-            self.conv3_2, self.relu3_2,
-            self.conv3_3, self.relu3_3,
-            self.pool3,
-            self.conv4_1, self.relu4_1,
-            self.conv4_2, self.relu4_2,
-            self.conv4_3, self.relu4_3,
-            self.pool4,
-            self.conv5_1, self.relu5_1,
-            self.conv5_2, self.relu5_2,
-            self.conv5_3, self.relu5_3,
-            self.pool5,
+        vgg_features = [
+            vgg16.features[0:4],
+            vgg16.features[5:9],
+            vgg16.features[10:16],
+            vgg16.features[17:23],
+            vgg16.features[24:29]
         ]
-        for l1, l2 in zip(vgg16.features, features):
-            if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
-                assert l1.weight.size() == l2.weight.size()
-                assert l1.bias.size() == l2.bias.size()
-                l2.weight.data = l1.weight.data
-                l2.bias.data = l1.bias.data
-        for i, name in zip([0, 3], ['fc6', 'fc7']):
-            l1 = vgg16.classifier[i]
-            l2 = getattr(self, name)
-            l2.weight.data = l1.weight.data.view(l2.weight.size())
-            l2.bias.data = l1.bias.data.view(l2.bias.size())
+        features = [
+            self.conv1.encode,
+            self.conv2.encode,
+            self.conv3.encode,
+            self.conv4.encode,
+            self.conv5.encode
+        ]
+        for l1, l2 in zip(vgg_features, features):
+            for i in range(len(list(l1.modules())) // 2):
+                assert isinstance(l1[i * 2], nn.Conv2d) == isinstance(l2[i * 3], nn.Conv2d)
+                assert l1[i * 2].weight.size() == l2[i * 3].weight.size()
+                assert l1[i * 2].bias.size() == l2[i * 3].bias.size()
+                l2[i * 3].weight.data = l1[i * 2].weight.data
+                l2[i * 3].bias.data = l1[i * 2].bias.data
