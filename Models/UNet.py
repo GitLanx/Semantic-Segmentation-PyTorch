bb444ae29b601754f77efd85ed07d2ab5572d153
GitLanx <675579465@qq.com>
2019-03-06 10:51:07 +0800

    fix error

--- Models/UNet.py
+++ Models/UNet.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 class EncoderBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(self).__init__()
+        super(EncoderBlock, self).__init__()
         layers = [
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
@@ -13,7 +13,6 @@ class EncoderBlock(nn.Module):
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
         ]
-        layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
         self.encode = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -22,7 +21,7 @@ class EncoderBlock(nn.Module):
 
 class DecoderBlock(nn.Module):
     def __init__(self, in_channels, middle_channels, out_channels):
-        super(self).__init__()
+        super(DecoderBlock, self).__init__()
         self.decode = nn.Sequential(
             nn.Conv2d(in_channels, middle_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(middle_channels),
@@ -38,33 +37,41 @@ class DecoderBlock(nn.Module):
 
 
 class UNet(nn.Module):
-    def __init__(self, num_classes):
-        super(self).__init__()
+    def __init__(self, n_classes):
+        super(UNet, self).__init__()
         self.enc1 = EncoderBlock(3, 64)
+        self.enc1_pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.enc2 = EncoderBlock(64, 128)
+        self.enc2_pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.enc3 = EncoderBlock(128, 256)
+        self.enc3_pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.enc4 = EncoderBlock(256, 512)
+        self.enc4_pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.center = DecoderBlock(512, 1024, 512)
         self.dec4 = DecoderBlock(1024, 512, 256)
         self.dec3 = DecoderBlock(512, 256, 128)
         self.dec2 = DecoderBlock(256, 128, 64)
         self.dec1 = nn.Sequential(
-            nn.Conv2d(128, 64, kernel_size=3),
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
         )
-        self.final = nn.Conv2d(64, num_classes, kernel_size=1)
+        self.final = nn.Conv2d(64, n_classes, kernel_size=1)
         initialize_weights(self)
 
     def forward(self, x):
         enc1 = self.enc1(x)
-        enc2 = self.enc2(enc1)
-        enc3 = self.enc3(enc2)
-        enc4 = self.enc4(enc3)
-        center = self.center(enc4)
+        enc1_pool = self.enc1_pool(enc1)
+        enc2 = self.enc2(enc1_pool)
+        enc2_pool = self.enc2_pool(enc2)
+        enc3 = self.enc3(enc2_pool)
+        enc3_pool = self.enc3_pool(enc3)
+        enc4 = self.enc4(enc3_pool)
+        enc4_pool = self.enc4_pool(enc4)
+        center = self.center(enc4_pool)
         dec4 = self.dec4(torch.cat([center, enc4], 1))
         dec3 = self.dec3(torch.cat([dec4, enc3], 1))
         dec2 = self.dec2(torch.cat([dec3, enc2], 1))
@@ -73,13 +80,12 @@ class UNet(nn.Module):
         return final
 
 
-def initialize_weights(*models):
-    for model in models:
-        for module in model.modules():
-            if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
-                nn.init.kaiming_normal_(module.weight)
-                if module.bias is not None:
-                    module.bias.data.zero_()
-            elif isinstance(module, nn.BatchNorm2d):
-                module.weight.data.fill_(1)
+def initialize_weights(model):
+    for module in model.modules():
+        if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+            nn.init.kaiming_normal_(module.weight)
+            if module.bias is not None:
                 module.bias.data.zero_()
+        elif isinstance(module, nn.BatchNorm2d):
+            module.weight.data.fill_(1)
+            module.bias.data.zero_()
