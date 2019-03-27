dd8ebf3d98dc7cd39f5b850fcb961273954cc402
GitLanx <675579465@qq.com>
2019-03-27 20:06:35 +0800

    clean code

--- Models/__init__.py
+++ Models/__init__.py
@@ -49,9 +49,9 @@ def model_loader(model_name, n_classes, resume):
         model.load_state_dict(checkpoint['model_state_dict'])
         start_epoch = checkpoint['epoch']
     else:
-        if model_name in ['fcn8s']:
-            vgg16 = models.vgg16(pretrained=True)
-            model.copy_params_from_vgg16(vgg16)
+    #     if model_name in ['fcn8s']:
+    #         vgg16 = models.vgg16(pretrained=True)
+    #         model.copy_params_from_vgg16(vgg16)
         checkpoint = None
 
     return model, start_epoch, checkpoint
