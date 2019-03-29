4c9cbf957d3dd34eced45a8cad36e74d233014d3
GitLanx <675579465@qq.com>
2019-03-29 20:15:42 +0800

    add optimizer

---
+++ optimizer.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+
+
+def get_optimizer(args, model):
+    if args.optim.lower() == 'sgd':
+        if args.model.lower() == 'deeplab-largefov':
+            optim = torch.optim.SGD(
+                [{'params': get_parameters(args, model, bias=False, final=False)},
+                 {'params': get_parameters(args, model, bias=True, final=False), 'lr': args.lr * 2, 'weight_decay': 0},
+                 {'params': get_parameters(args, model, bias=False, final=True), 'lr': args.lr * 10},
+                 {'params': get_parameters(args, model, bias=True, final=True), 'lr': args.lr * 20, 'weight_decay': 0}],
+                lr=args.lr,
+                momentum=args.beta1,
+                weight_decay=args.weight_decay)
+        else:
+            optim = torch.optim.SGD(
+                model.parameters(),
+                lr=args.lr,
+                momentum=args.beta1,
+                weight_decay=args.weight_decay)
+
+    elif args.optim.lower() == 'adam':
+        optim = torch.optim.Adam(
+            model.parameters(),
+            lr=args.lr,
+            betas=(args.beta1, 0.999),
+            weight_decay=args.weight_decay)
+    
+    return optim
+
+def get_parameters(args, model, bias=False, final=False):
+    """Adapted from:
+
+    https://github.com/BardOfCodes/pytorch_deeplab_large_fov/blob/master/utils.py
+    """
+    if final:
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d):
+                if m.out_channels == args.n_classes:
+                    if bias:
+                        yield m.bias
+                    else:
+                        yield m.weight
+    else:
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d):
+                if not m.out_channels == args.n_classes: 
+                    if bias:
+                        yield m.bias
+                    else:
+                        yield m.weight
