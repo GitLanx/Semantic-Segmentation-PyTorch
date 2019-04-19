270fd622ce4c71761c0f0f924d7de71b61359d5a
Lanxin <675579465@qq.com>
2019-04-19 20:16:44 +0800

    format code

--- Dataloader/baseloader.py
+++ Dataloader/baseloader.py
@@ -1,24 +1,23 @@
 import numpy as np
-from PIL import Image
 import torch
 from torch.utils import data
 from torchvision import transforms
+
 
 class BaseLoader(data.Dataset):
     # specify class_name if available
     class_name = None
 
-    def __init__(
-        self,
-        root,
-        n_classes,
-        split='train',
-        img_size=None,
-        augmentations=None,
-        ignore_index=None,
-        class_weight=None,
-        pretrained=False
-        ):
+    def __init__(self,
+                 root,
+                 n_classes,
+                 split='train',
+                 img_size=None,
+                 augmentations=None,
+                 ignore_index=None,
+                 class_weight=None,
+                 pretrained=False):
+
         self.root = root
         self.n_classes = n_classes
         self.split = split
@@ -31,22 +30,19 @@ class BaseLoader(data.Dataset):
             # if use pretrained model, substract mean and divide standard deviation
             self.mean = torch.tensor([0.485, 0.456, 0.406])
             self.std = torch.tensor([0.229, 0.224, 0.225])
-            self.tf = transforms.Compose(
-                [transforms.ToTensor(),
-                transforms.Normalize(self.mean.tolist(), self.std.tolist())]
-            )
-            self.untf = transforms.Compose(
-                [transforms.Normalize((-self.mean / self.std).tolist(),
-                                    (1.0 / self.std).tolist())]
-            )
+            self.tf = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(self.mean.tolist(), self.std.tolist())
+            ])
+            self.untf = transforms.Compose([
+                transforms.Normalize((-self.mean / self.std).tolist(),
+                                     (1.0 / self.std).tolist())
+            ])
         else:
             # if not use pretrained model, only scale images to [0, 1]
-            self.tf = transforms.Compose(
-                [transforms.ToTensor()]
-            )
+            self.tf = transforms.Compose([transforms.ToTensor()])
             self.untf = transforms.Compose(
-                [transforms.Normalize([0, 0, 0], [1, 1, 1])]
-            )
+                [transforms.Normalize([0, 0, 0], [1, 1, 1])])
 
     def __getitem__(self, index):
         return NotImplementedError
@@ -67,6 +63,6 @@ class BaseLoader(data.Dataset):
         img = img.astype(np.uint8)
         lbl = lbl.numpy()
         return img, lbl
-    
+
     def getpalette(self):
         return NotImplementedError
