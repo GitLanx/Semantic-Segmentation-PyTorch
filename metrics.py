efac50dcd6dd72c7c98719753480acf35d37394f
Lanxin <675579465@qq.com>
2019-04-19 20:42:39 +0800

    add multiscale metrics

---
+++ metrics.py
@@ -0,0 +1,81 @@
+import numpy as np
+import torch.nn.functional as F
+
+
+# Adapted from:
+# https://github.com/meetshah1995/pytorch-semseg/blob/master/ptsemseg/metrics.py
+
+
+class runningScore(object):
+    def __init__(self, n_classes):
+        self.n_classes = n_classes
+        self.confusion_matrix = np.zeros((n_classes, n_classes))
+
+    def _fast_hist(self, label_true, label_pred, n_class):
+        mask = (label_true >= 0) & (label_true < n_class)
+        hist = np.bincount(
+            n_class * label_true[mask].astype(int) + label_pred[mask],
+            minlength=n_class**2).reshape(n_class, n_class)
+        return hist
+
+    def update(self, label_trues, label_preds):
+        for lt, lp in zip(label_trues, label_preds):
+            self.confusion_matrix += self._fast_hist(
+                lt.flatten(), lp.flatten(), self.n_classes)
+
+    def get_scores(self):
+        """Returns accuracy score evaluation result.
+            - overall accuracy
+            - mean accuracy
+            - mean IU
+            - fwavacc
+        """
+        hist = self.confusion_matrix
+        acc = np.diag(hist).sum() / hist.sum()
+        acc_cls = np.diag(hist) / hist.sum(axis=1)
+        acc_cls = np.nanmean(acc_cls)
+        iu = np.diag(hist) / (
+            hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist))
+        mean_iu = np.nanmean(iu)
+        freq = hist.sum(axis=1) / hist.sum()
+        fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
+        cls_iu = dict(zip(range(self.n_classes), iu))
+
+        return acc, acc_cls, mean_iu, fwavacc, cls_iu
+
+    def reset(self):
+        self.confusion_matrix = np.zeros((self.n_classes, self.n_classes))
+
+
+class averageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def get_multiscale_results(score, target, upsample_logits=False):
+    if upsample_logits:
+        score = F.interpolate(score, size=target.size[2:], mode='bilinear', align_corners=True)
+    else:
+        target = F.interpolate(target, size=score.size[2:], mode='nearest', align_corners=True)
+
+    if not isinstance(score, tuple):
+        lbl_pred = score.data.max(1)[1].cpu().numpy()
+    else:
+        lbl_pred = score[-1].data.max(1)[1].cpu().numpy()
+
+    lbl_true = target.data.cpu().numpy()
+    return lbl_pred, lbl_true
