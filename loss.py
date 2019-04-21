bbf00e94b2d07c9d3ac2616597231ff5a3741815
GitLanx <675579465@qq.com>
2019-04-21 08:50:06 +0800

    fix error

--- loss.py
+++ loss.py
@@ -1,16 +1,26 @@
 import torch.nn.functional as F
+import numpy as np
+from PIL import Image
+import torch
 
 
 def CrossEntropyLoss(score, target, weight, ignore_index, reduction):
-    if not isinstance(score, tuple):
+    if not isinstance(score, list):
         loss = F.cross_entropy(
-            score, target, weight=weight, ignore_index=-1, reduction='mean')
+            score, target, weight=weight, ignore_index=ignore_index, reduction=reduction)
         return loss
 
     loss = 0
     for s in score:
-        target = F.interpolate(
-            target, size=s.size[1:], mode='nearest', align_corners=True)
         loss = loss + F.cross_entropy(
-            score, target, weight=weight, ignore_index=-1, reduction='mean')
+            s, target, weight=weight, ignore_index=ignore_index, reduction=reduction)
     return loss
+
+def resize_labels(labels, size):
+    new_labels = []
+    for label in labels:
+        label = label.float().cpu().numpy()
+        label = Image.fromarray(label).resize((size[1], size[0]), Image.NEAREST)
+        new_labels.append(np.asarray(label))
+    new_labels = torch.LongTensor(new_labels)
+    return new_labels
